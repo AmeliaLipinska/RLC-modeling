3bc68d8d1e092bfbe33e16473949747e863a393a
alipinska <a.lipinska2005@wp.pl>
2026-04-27 20:54:06 +0200

    made matrixes with np.array

--- model.py
+++ model.py
@@ -5,13 +5,19 @@ from scipy import signal
 def circuit_model(L,R2,C,R,x0,x,u):
     #state-space model
 
-    model_A = [[0,0],[0,-1/C * (1/R + 1/R2)]]
+    model_A = np.array[[0,0],[0,-1/C * (1/R + 1/R2)]]
 
-    model_B = [[1/L],[1/(C*R2)]]
+    model_B = np.array[[1/L],[1/(C*R2)]]
 
-    model_C = [0,1]
+    model_C = np.array[0,1]
 
     model_D = 0
+
+    # x to wekror stanu x=[x1, x2] 
+    # gdzie x1=prąd cewki, 
+    # x2=napięcie na rezystorze=y(t)
+
+    # @-operator mnożenia macierzy 
 
     dxdt = model_A @ x + model_B.flatten() * u
     y = model_C * x + model_D
